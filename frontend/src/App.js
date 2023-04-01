ee6d41d47d2ff8ef0ba109d8c323e4d36cd02cc9
yunchiapig <carolwi204@gmail.com>
2023-04-02 00:12:17 +0800

    frontend init

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Card from "./Components/ShopInfoCard";
+import ProductAddToCart from './Components/Card';
+
+function App() {
+  return (
+    <div className="App">
+      <Card/>
+      <ProductAddToCart/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/ShopInfoCard.js
@@ -0,0 +1,22 @@
+
+function Card(){
+    return (
+        <div className="card">
+            <div className="shop-details">
+                <div className="shop-name">台灣大學</div>
+                <div className="shop-address">台北市大安區羅斯福路四段1號</div>
+                {/* distance */}
+                {/* like button */}
+            </div>
+            <div className="item-categories"></div>
+                <div className="category">
+                    {/* icon */}
+                    <div className="category-name">蔬果</div>
+                </div>
+            <div className="item-details"></div>
+        </div>
+    )
+
+};
+
+export default Card;
