bfcc3d4e345f61e18b56dca59fb7b128c7f97025
“minchenlee” <110967458+minchenlee@users.noreply.github.com>
2023-04-02 22:16:12 +0800

    feat: add Store Admin API and more
    1. Created backend using express
    2. Added Store Admin API.
    3. Added swagger.yaml for API documentation
    3. Added healthcheck page for checking server's health

---
+++ backend/model/store_info.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+// 連線到 mongoDB
+const connectDB = require('./DB_connection');
+connectDB();
+
+// create store info schema
+let storeInfoSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  tel: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  longitude: {
+    type: Number,
+    required: true
+  },
+  latitude: {
+    type: Number,
+    required: true
+  },
+  updateDate: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  stock_id: {
+    type: Array,
+  }
+});
+
+// create store info model
+const StoreInfo = mongoose.model('store', storeInfoSchema, 'store');
+module.exports = StoreInfo;
