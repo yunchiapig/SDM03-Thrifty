a6c7a8fdcae26eae901d843e49047b5212e71af4
“minchenlee” <110967458+minchenlee@users.noreply.github.com>
2023-04-05 17:39:40 +0800

    fix: fix food price format check rule and more
    1. Fix food price format check rule.
    2. Update api endpoints.

--- backend/app.js
+++ backend/app.js
@@ -42,13 +42,13 @@ app.use('/healthcheck', healthcheckRouter);
 app.use('/users', usersRouter);
 
 // admin routers
-app.use('/admin/store', storeAdminRouter);
-app.use('/admin/food', foodAdminRouter);
-app.use('/admin/stock', stockAdminRouter);
+app.use('/api/1.0/admin/store', storeAdminRouter);
+app.use('/api/1.0/admin/food', foodAdminRouter);
+app.use('/api/1.0/admin/stock', stockAdminRouter);
 
 // get routers
-app.use('/get/stores', getStoresRouter);
-app.use('/get/foods', getFoodsRouter);
+app.use('/api/1.0/get/stores', getStoresRouter);
+app.use('/api/1.0/get/foods', getFoodsRouter);
 
 // swagger ui
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
