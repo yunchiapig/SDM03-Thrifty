ded51b8656dc562b934bf6f6386cd8ac71420776
“minchenlee” <110967458+minchenlee@users.noreply.github.com>
2023-04-09 21:25:10 +0800

    refactor: using cors on app level
    1. Using cors on app level.

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors');
 
 // using swagger
 const swaggerUi = require('swagger-ui-express');
@@ -36,6 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 // routers
 app.use('/healthcheck', healthcheckRouter);
--- backend/routes/get_stores_api.js
+++ backend/routes/get_stores_api.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const cors = require('cors');
-router.use(cors());
 
 // 引入 StoreInfo model
 const StoreInfo = require('../model/store_info');
