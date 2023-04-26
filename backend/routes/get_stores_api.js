dba4a0a66ed20d123c672a4ac0af187391fb7b59
champion <champion8866@gmail.com>
2023-04-26 17:29:14 +0800

    update get_stores_api, which returns the restaurants with non-empty stocks and adds more information to each stock

--- backend/routes/get_stores_api.js
+++ backend/routes/get_stores_api.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// 引入 StoreInfo model
+// 引入 FoodINfo 和 StoreInfo model
+const FoodInfo = require('../model/food_info');
 const StoreInfo = require('../model/store_info');
 
 // 引入檢查格式的 middleware function
@@ -16,16 +17,31 @@ router.get('/', checkUserLocation, async function (req, res) {
               $near: {
                   $geometry: { type: "Point",  coordinates: [ longitude, latitude ] },
                   $minDistance: 0, // meters
-                  $maxDistance: 1000 // meters
+                  $maxDistance: 2000 // meters
               }
-          }
+          },
+          stocks: { $ne: [] }
         })
-      
-        if (store_exist.length == 0) {
-          res.status(400).send({ message: "使用者周圍地區無商家" });
-        } else {
-          res.status(200).send({ message: store_exist })
+
+      for (const store of store_exist) {
+        for (const stock of store.stocks) {
+          const food_id = stock._id
+          const food_exist = await FoodInfo.findById(food_id)
+          if (!food_exist) {
+            console.log("Food not found!")
+          } else {
+            stock.name = food_exist.name
+            stock.category = food_exist.category
+            stock.discount_price = food_exist.discount_price
+          }
         }
+      }
+      
+      if (store_exist.length == 0) {
+        res.status(400).send({ message: "使用者周圍地區無商家" });
+      } else {
+        res.status(200).send({ message: store_exist })
+      }
     } catch(error) {
       console.log(error);
       res.status(500).send({ message: "Internal Server Error!" });
